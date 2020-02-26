e0997a1daa327b65080001ea09b636071d341ea6
Dat Vu <vd.thanhdat@gmail.com>
2020-02-27 01:19:26 +0700

    Update form

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Post
 # Create your views here.
@@ -11,5 +11,10 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
 
 
